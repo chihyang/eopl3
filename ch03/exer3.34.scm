b1badc7643ad585480dcd18e5a6f4128a0207c44
chihyang <chihyanghsin@gmail.com>
2019-02-25 22:24:45 +0800

    Exercise 3.34.

---
+++ ch03/exer3.34.scm
@@ -0,0 +1,234 @@
+#lang eopl
+;;; ---------------------- Environment(from section 3.2) ----------------------
+(define empty-env
+  (lambda ()
+    (lambda (search-var)
+      (report-no-binding-found search-var))))
+(define extend-env
+  (lambda (saved-var saved-val saved-env)
+    (lambda (search-var)
+      (if (eqv? search-var saved-var)
+          saved-val
+          (apply-env saved-env search-var)))))
+(define extend-env-list
+  (lambda (saved-vars saved-vals saved-env)
+    (lambda (search-var)
+      (cond ((null? saved-vals)
+             (apply-env saved-env search-var))
+            ((eqv? search-var (car saved-vars))
+             (car saved-vals))
+            (else
+             (apply-env (extend-env-list (cdr saved-vars) (cdr saved-vals) saved-env)
+                        search-var))))))
+(define extend-env-rec
+  (lambda (p-name p-var p-body saved-env)
+    (lambda (search-var)
+      (if (eqv? search-var p-name)
+          (proc-val
+           (procedure p-var
+                      p-body
+                      (extend-env-rec p-name p-var p-body saved-env)))
+          (apply-env saved-env search-var)))))
+(define apply-env
+  (lambda (env search-var)
+    (env search-var)))
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env "No binding for ~s" search-var)))
+(define report-invalid-env
+  (lambda (env)
+    (eopl:error 'apply-env "Bad environment: ~s" env)))
+
+;;; ---------------------- Expval ----------------------
+(define identifier? symbol?)
+;;; proc? : SchemeVal -> Bool
+(define proc?
+  (lambda (val)
+    (procedure? val)))
+;;; procedure : Var x Exp x Env -> Proc
+(define procedure
+  (lambda (var body env)
+    (lambda (val)
+      (value-of body (extend-env var val env)))))
+;;; apply-procedure : Proc x ExpVal -> ExpVal
+(define apply-procedure
+  (lambda (proc1 val)
+    (proc1 val)))
+(define-datatype exp-val exp-val?
+  (num-val
+   (val number?))
+  (bool-val
+   (val boolean?))
+  (proc-val
+   (val proc?)))
+(define expval->num
+  (lambda (value)
+    (cases exp-val value
+           (num-val
+            (number)
+            number)
+           (else
+            (report-invalid-exp-value 'num)))))
+(define expval->bool
+  (lambda (value)
+    (cases exp-val value
+           (bool-val
+            (boolean)
+            boolean)
+           (else
+            (report-invalid-exp-value 'bool)))))
+(define expval->proc
+  (lambda (value)
+    (cases exp-val value
+           (proc-val
+            (proc1)
+            proc1)
+           (else
+            (report-invalid-exp-value 'proc)))))
+(define report-invalid-exp-value
+  (lambda (type)
+    (eopl:error
+     'exp-val
+     "No a valid exp value of type ~s" type)))
+
+;;; ---------------------- Syntax for the PROC language ----------------------
+;;; Program    ::= Expression
+;;;                a-program (exp1)
+;;; Expression ::= Number
+;;;                const-exp (num)
+;;; Expression ::= -(Expression , Expression)
+;;;                diff-exp (exp1 exp2)
+;;; Expression ::= zero? (Expression)
+;;;                zero?-exp (exp1)
+;;; Expression ::= if Expression then Expression else Expression
+;;;                if-exp (exp1 exp2 exp3)
+;;; Expression ::= Identifier
+;;;                var-exp (var)
+;;; Expression ::= let {Identifier = Expression}* in Expression
+;;;                let-exp (var exp1 body)
+;;; Expression ::= proc (Identifier) Expression
+;;;                proc-exp (var body)
+;;; Expression ::= letrec Identifier (Identifier) = Expression in Expression
+;;;                let-exp (var exp1 body)
+;;; Expression ::= (Expression Expression)
+;;;                call-exp (rator rand)
+;;; Parse Expression
+(define let-scanner-spec
+  '((white-sp (whitespace) skip)
+    (identifier (letter (arbno (or letter digit))) symbol)
+    (number ((or (concat digit (arbno digit))
+                 (concat "-" digit (arbno digit))
+                 (concat (arbno digit) "." digit (arbno digit))
+                 (concat "-" (arbno digit) "." digit (arbno digit))
+                 (concat digit (arbno digit) "." (arbno digit))
+                 (concat "-" digit (arbno digit) "." (arbno digit)))) number)))
+(define let-grammar
+  '((program (expression) a-program)
+    (expression (number)
+                const-exp)
+    (expression ("-" "(" expression "," expression ")")
+                diff-exp)
+    (expression ("zero?" "(" expression ")")
+                zero?-exp)
+    (expression ("if" expression "then" expression "else" expression)
+                if-exp)
+    (expression (identifier)
+                var-exp)
+    (expression ("let" (arbno identifier "=" expression) "in" expression)
+                let-exp)
+    (expression ("proc" "(" identifier ")" expression)
+                proc-exp)
+    (expression ("letrec" identifier "(" identifier ")" "=" expression "in" expression)
+                letrec-exp)
+    (expression ("(" expression expression ")")
+                call-exp)))
+
+;;; ---------------------- Evaluate expression ----------------------
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+           (const-exp
+            (num)
+            (num-val num))
+           (var-exp
+            (var)
+            (apply-env env var))
+           (diff-exp
+            (exp1 exp2)
+            (num-val (- (expval->num (value-of exp1 env))
+                        (expval->num (value-of exp2 env)))))
+           (zero?-exp
+            (exp1)
+            (bool-val (eqv? (expval->num (value-of exp1 env)) 0)))
+           (if-exp
+            (exp1 exp2 exp3)
+            (if (expval->bool (value-of exp1 env))
+                (value-of exp2 env)
+                (value-of exp3 env)))
+           (let-exp
+            (var exp1 body)
+            (value-of-let-exp var exp1 body env))
+           (proc-exp
+            (var body)
+            (proc-val (procedure var body env)))
+           (letrec-exp
+            (p-name p-vars p-body letrec-body)
+            (value-of letrec-body (extend-env-rec p-name p-vars p-body env)))
+           (call-exp
+            (rator rand)
+            (let ((proc (expval->proc (value-of rator env)))
+                  (arg (value-of rand env)))
+              (apply-procedure proc arg))))))
+(define value-of-let-exp
+  (lambda (var exp body env)
+    (value-of
+     body
+     (let ((values (map (lambda (val) (value-of val env)) exp)))
+       (extend-env-list var values env)))))
+(define value-of--program
+  (lambda (prog)
+    (cases program prog
+           (a-program
+            (exp)
+            (let ((val (value-of exp (empty-env))))
+              (cases exp-val val
+                     (num-val
+                      (num)
+                      num)
+                     (bool-val
+                      (bool)
+                      bool)
+                     (proc-val
+                      (val)
+                      val)))))))
+
+;;; ---------------------- Sllgen operations ----------------------
+(sllgen:make-define-datatypes let-scanner-spec let-grammar)
+(define list-the-datatypes
+  (lambda ()
+    (sllgen:list-define-datatypes let-scanner-spec let-grammar)))
+(define just-scan
+  (sllgen:make-string-scanner let-scanner-spec let-grammar))
+(define scan&parse
+  (sllgen:make-string-parser let-scanner-spec let-grammar))
+(define read-eval-print
+  (sllgen:make-rep-loop
+   "--> "
+   value-of--program
+   (sllgen:make-stream-parser let-scanner-spec let-grammar)))
+(define run
+  (lambda (exp)
+    (value-of--program (scan&parse exp))))
+
+;;; ---------------------- Test ----------------------
+(eqv?
+ (run "letrec double (x) = if zero?(x) then 0
+                           else -((double -(x,1)),-2)
+       in (double 6)")
+ 12)
+(eqv?
+ (run "let a = 3
+       in let p = proc (x) -(x,a)
+              a = 5
+          in -(a, (p 2))")
+ 6)
