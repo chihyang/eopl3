34954086513553a7f3175d1c32cce97b31799e3c
chihyang <chihyanghsin@gmail.com>
2019-07-02 23:31:07 +0800

    Another version of exercise 5.51.

--- ch05/exer5.51.v1.scm
+++ ch05/exer5.51.v1.scm
@@ -42,3 +42,35 @@
     44)
    (eopl:printf "~%"))
  '(1 10 100))
+
+;;; with yield, however, the consumer could give up its CPU time slice, thus
+;;; making loosewait unnecessary
+(map
+ (lambda (d)
+   (check-eqv?
+    (run "let buffer = 0
+           mut = mutex()
+       in let producer = proc (n)
+              letrec
+                wait4(k) = if zero?(k)
+                           then set buffer = n
+                           else begin
+                                  print(-(k,-200));
+                                  (wait4 -(k,1))
+                                end
+                in begin wait(mut); (wait4 5); signal(mut) end
+           in let consumer = proc ()
+                              begin
+                               yield();
+                               wait(mut);
+                               buffer
+                              end
+              in begin
+                  spawn(proc (d) (producer 44));
+                  print(300);
+                  (consumer)
+                 end"
+         #:time-slice d)
+    44)
+   (eopl:printf "~%"))
+ '(1 10 100))
