93248dc95d8eaa5adf60ab0623bc043b814693a5
chihyang <chihyanghsin@gmail.com>
2019-08-04 16:24:39 +0800

    Exercise 6.22 and 6.23.

---
+++ ch06/exer06.22.tests.scm
@@ -0,0 +1,31 @@
+#lang eopl
+(require "cps-in-lang.scm")
+(require "exer06.22.scm")
+(require "cps-interp.scm")
+
+(require rackunit)
+(require "cps-tests.scm")
+
+(define test-name car)
+(define test-program cadr)
+(define test-answer caddr)
+(define passed 0)
+(define failed 0)
+
+(for-each
+ (lambda (test)
+   (let ((v1 (checked-run
+              (compile (scan&parse (test-program test)))))
+         (v2 (test-answer test)))
+     (if  (equal? v1 v2)
+          (begin
+            (set! passed (+ passed 1)))
+          (begin
+            (set! failed (+ failed 1))
+            (eopl:printf "test for ~a failed: expect ~a, actual ~a~%"
+                         (test-name test) v2 v1)))))
+ test-list)
+
+(if (eq? passed (length test-list))
+    (eopl:printf "all tests passed!~%")
+    (eopl:printf "~%~a tests failed!~%" failed))
