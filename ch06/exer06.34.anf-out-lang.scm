3739b08114a30b6b260cb53cde6bb7784a0b8280
chihyang <chihyanghsin@gmail.com>
2019-08-12 13:00:14 +0800

    Exercise 6.34.
    ANF part is completed, interpreter part will be completed soon.

---
+++ ch06/exer06.34.anf-out-lang.scm
@@ -0,0 +1,141 @@
+#lang eopl
+(provide (all-defined-out))
+;;; ---------------------- Syntax for the ANF-OUT language ----------------------
+;;; TfProgram  ::= TfExp
+;;;                anf-a-program (exp1)
+;;; SimpleExp  ::= Number
+;;;                anf-const-exp (num)
+;;; SimpleExp  ::= Identifier
+;;;                anf-var-exp (var)
+;;; SimpleExp  ::= + (SimpleExp{,}*)
+;;;                anf-sum-exp (exps)
+;;; SimpleExp  ::= - (SimpleExp, SimpleExp)
+;;;                anf-diff-exp (exp1, exp2)
+;;; SimpleExp  ::= zero? (SimpleExp)
+;;;                anf-zero?-exp (exp1)
+;;; SimpleExp  ::= less? (SimpleExp, SimpleExp)
+;;;                anf-less?-exp (exp1, exp2)
+;;; TfExp      ::= SimpleExp
+;;;                simple-exp->exp (simple-exp1)
+;;; TfExp      ::= if SimpleExp then TfExp else TfExp
+;;;                anf-if-exp (exp1 exp2 exp3)
+;;; TfExp      ::= let {Identifier = SimpleExp}* in TfExp
+;;;                anf-let-exp (vars exps body)
+;;; TfExp      ::= letrec {Identifier (Identifier*,) = TfExp}* in TfExp
+;;;                anf-letrec-exp (p-names b-vars p-bodies body)
+;;; TfExp      ::= (SimpleExp SimpleExp*)
+;;;                anf-call-exp (rator rands)
+;;; Parse Expression
+(define anf-scanner-spec
+  '((white-sp (whitespace) skip)
+    (identifier (letter (arbno (or letter digit "_" "-" "?"))) symbol)
+    (number ((or (concat digit (arbno digit))
+                 (concat "-" digit (arbno digit))
+                 (concat (arbno digit) "." digit (arbno digit))
+                 (concat "-" (arbno digit) "." digit (arbno digit))
+                 (concat digit (arbno digit) "." (arbno digit))
+                 (concat "-" digit (arbno digit) "." (arbno digit)))) number)
+    (comment ("%" (arbno (not #\newline))) skip)))
+(define anf-grammar
+  '((anf-program (tf-exp) anf-a-program)
+    (simple-exp (number)
+                anf-const-exp)
+    (simple-exp (identifier)
+                anf-var-exp)
+    (simple-exp ("proc" "(" (separated-list identifier ",") ")" tf-exp)
+                anf-proc-exp)
+    (simple-exp ("+" "(" (separated-list simple-exp ",") ")")
+                anf-sum-exp)
+    (simple-exp ("-" "(" simple-exp "," simple-exp ")")
+                anf-diff-exp)
+    (simple-exp ("zero?" "(" simple-exp ")")
+                anf-zero?-exp)
+    (simple-exp ("less?" "(" simple-exp "," simple-exp ")")
+                anf-less?-exp)
+    (anf-call-exp ("(" simple-exp (arbno simple-exp) ")")
+                  anf-a-call-exp)
+    (anf-simple-exp (simple-exp)
+                    anf-simple-exp->exp)
+    (anf-simple-exp (anf-call-exp)
+                    anf-call-exp->exp)
+    (tf-exp (anf-simple-exp)
+            simple-exp->exp)
+    (tf-exp ("if" simple-exp "then" tf-exp "else" tf-exp)
+            anf-if-exp)
+    (tf-exp ("let" (arbno identifier "=" anf-simple-exp) "in" tf-exp)
+            anf-let-exp)
+    (tf-exp ("letrec" (arbno identifier "(" (separated-list identifier ",") ")" "=" tf-exp) "in" tf-exp)
+            anf-letrec-exp)))
+
+(sllgen:make-define-datatypes anf-scanner-spec anf-grammar)
+(define anf-list-the-datatypes
+  (lambda ()
+    (sllgen:list-define-datatypes anf-scanner-spec anf-grammar)))
+(define anf-just-scan
+  (sllgen:make-string-scanner anf-scanner-spec anf-grammar))
+(define anf-scan&parse
+  (sllgen:make-string-parser anf-scanner-spec anf-grammar))
+
+(define anf-unparse-simple-exp
+  (lambda (exp)
+    (cases simple-exp exp
+           (anf-const-exp (num) num)
+           (anf-var-exp (var) var)
+           (anf-proc-exp
+            (vars body)
+            `(lambda ,vars ,(anf-unparse-exp body)))
+           (anf-sum-exp
+            (exps)
+            (append '(+) (map anf-unparse-simple-exp exps)))
+           (anf-diff-exp
+            (exp1 exp2)
+            `(- ,(anf-unparse-simple-exp exp1) ,(anf-unparse-simple-exp exp2)))
+           (anf-zero?-exp
+            (exp1)
+            `(zero? ,(anf-unparse-simple-exp exp1)))
+           (anf-less?-exp
+            (exp1 exp2)
+            `(< ,(anf-unparse-simple-exp exp1) ,(anf-unparse-simple-exp exp2))))))
+
+(define anf-unparse-anf-simple-exp
+  (lambda (exp)
+    (cases anf-simple-exp exp
+           (anf-simple-exp->exp (simple) (anf-unparse-simple-exp simple))
+           (anf-call-exp->exp (call) (anf-unparse-anf-call-exp call)))))
+
+(define anf-unparse-anf-call-exp
+  (lambda (exp)
+    (cases anf-call-exp exp
+           (anf-a-call-exp
+            (rator rands)
+            (map anf-unparse-simple-exp (cons rator rands))))))
+
+(define anf-unparse-exp
+  (lambda (exp)
+    (cases tf-exp exp
+           (simple-exp->exp (simple) (anf-unparse-anf-simple-exp simple))
+           (anf-if-exp
+            (exp1 exp2 exp3)
+            `(if ,(anf-unparse-simple-exp exp1)
+                 ,(anf-unparse-exp exp2)
+                 ,(anf-unparse-exp exp3)))
+           (anf-let-exp
+            (vars exps body)
+            `(let ,(map (lambda (v1 v2) (list v1 (anf-unparse-anf-simple-exp v2)))
+                        vars exps)
+               ,(anf-unparse-exp body)))
+           (anf-letrec-exp
+            (p-names p-vars p-bodies body)
+            `(letrec ,(map (lambda (v1 v2 v3)
+                             (list v1
+                                   `(lambda ,v2
+                                      ,(anf-unparse-exp v3))))
+                        p-names p-vars p-bodies)
+               ,(anf-unparse-exp body))))))
+
+(define anf-unparse-prgm
+  (lambda (prgm)
+    (cases anf-program prgm
+           (anf-a-program
+            (exp)
+            (anf-unparse-exp exp)))))
---
+++ ch06/exer06.34.scm
@@ -0,0 +1,233 @@
+#lang eopl
+(require "exer06.34.anf-in-lang.scm")
+(require "exer06.34.anf-out-lang.scm")
+(provide compile)
+
+;;; list-index : pred x list -> Int | #f
+;;;
+;;; usage: return the 0-based position of the first element of lst that
+;;; satisfies the predicate pred. If no element of lst satisfies the predicate,
+;;; then returns #f
+(define list-index
+  (lambda (pred lst)
+    (letrec ((list-index-iter
+              (lambda (pred lst n)
+                (if (null? lst)
+                    #f
+                    (if (pred (car lst))
+                        n
+                        (list-index-iter pred (cdr lst) (+ n 1)))))))
+      (list-index-iter pred lst 0))))
+
+;;; list-set : list x n x x -> list
+;;;
+;;; usage: return a list with nth-element in lst replaced by x
+(define list-set
+  (lambda (lst n x)
+    (if (null? lst)
+        '()
+        (if (eq? n 0)
+            (cons x (cdr lst))
+            (cons (car lst)
+                  (list-set (cdr lst) (- n 1) x))))))
+
+;;; ---------------------- ANF ----------------------
+(define end-cont
+  (lambda (v0) v0))
+
+(define anf-of-program
+  (lambda (prgm)
+    (set! n 0)
+    (cases program prgm
+           (a-program
+            (exp)
+            (anf-a-program
+             (anf-of-exp exp end-cont))))))
+
+(define compile anf-of-program)
+
+(define anf-of-exp
+  (lambda (exp k)
+    (cases expression exp
+           (const-exp
+            (num)
+            (make-send-to-cont k (anf-const-exp num)))
+           (var-exp
+            (var)
+            (make-send-to-cont k (anf-var-exp var)))
+           (proc-exp
+            (vars body)
+            (make-send-to-cont k (anf-proc-exp
+                                  vars
+                                  (anf-of-exp body '()))))
+           (zero?-exp
+            (exp1)
+            (anf-of-exps
+             (list exp1)
+             (lambda (simples)
+               (make-send-to-cont k (anf-zero?-exp (car simples))))))
+           (less?-exp
+            (exp1 exp2)
+            (anf-of-exps
+             (list exp1 exp2)
+             (lambda (simples)
+               (make-send-to-cont k (anf-less?-exp (car simples) (cadr simples))))))
+           (diff-exp
+            (exp1 exp2)
+            (anf-of-exps
+             (list exp1 exp2)
+             (lambda (simples)
+               (make-send-to-cont k (anf-diff-exp (car simples) (cadr simples))))))
+           (sum-exp
+            (exps)
+            (anf-of-exps
+             exps
+             (lambda (simples)
+               (make-send-to-cont k (anf-sum-exp simples)))))
+           (if-exp
+            (exp1 exp2 exp3)
+            (anf-of-exps (list exp1)
+                         (lambda (simples)
+                           (anf-if-exp
+                            (car simples)
+                            (anf-of-exp exp2 k)
+                            (anf-of-exp exp3 k)))))
+           (let-exp
+            (vars exp1 body)
+            (anf-of-exps
+             exp1
+             (lambda (simples)
+               (anf-let-exp vars
+                            simples
+                            (anf-of-exp body)))))
+           (letrec-exp
+            (p-names p-vars p-bodies body)
+            (anf-letrec-exp
+             p-names
+             p-vars
+             (map (lambda (e) (anf-of-exp e k)) p-bodies)
+             (anf-of-exp body k)))
+           (call-exp
+            (rator rands)
+            (anf-of-exps (cons rator rands)
+                         (lambda (simples)
+                           (make-call-send-to-cont
+                            k
+                            (anf-a-call-exp (car simples)
+                                            (cdr simples)))))))))
+
+;;; anf-of-exps: Listof(InpExp) x (Listof(InpExp) -> TfExp) -> TfExp
+(define anf-of-rest
+  (lambda (exps builder)
+    (let ((pos (list-index
+                (lambda (exp)
+                  (not (inp-exp-simple? exp)))
+                exps)))
+      (if (not pos)
+          (builder (map anf-of-simple-exp exps))
+          (let ((var (fresh-identifier 'v)))
+            (anf-of-exp
+             (list-ref exps pos)
+             (anf-proc-exp (list var)
+                           (anf-of-rest
+                            (list-set exps pos (var-exp var))
+                            builder))))))))
+
+(define anf-of-exps
+  (lambda (exps builder)
+    (anf-of-rest exps builder)))
+
+;;; inp-exp-simple? : InpExp -> Bool
+(define inp-exp-simple?
+  (lambda (exp)
+    (cases expression exp
+           (const-exp (num) #t)
+           (var-exp (var) #t)
+           (diff-exp
+            (exp1 exp2)
+            (and (inp-exp-simple? exp1)
+                 (inp-exp-simple? exp2)))
+           (zero?-exp
+            (exp1)
+            (inp-exp-simple? exp1))
+           (less?-exp
+            (exp1 exp2)
+            (and (inp-exp-simple? exp1)
+                 (inp-exp-simple? exp2)))
+           (proc-exp
+            (ids exp)
+            #t)
+           (sum-exp
+            (exps)
+            ((list-of inp-exp-simple?) exps))
+           (else #f))))
+
+;;; anf-of-simple-exp : InpExp -> SimpleExp
+;;; usage: assumes (inp-exp-simple exp).
+(define anf-of-simple-exp
+  (lambda (exp)
+    (cases expression exp
+           (const-exp (num) (anf-const-exp num))
+           (var-exp (var) (anf-var-exp var))
+           (diff-exp (exp1 exp2)
+                     (anf-diff-exp
+                      (anf-of-simple-exp exp1)
+                      (anf-of-simple-exp exp2)))
+           (zero?-exp (exp1)
+                      (anf-zero?-exp (anf-of-simple-exp exp1)))
+           (less?-exp (exp1 exp2)
+                      (anf-less?-exp
+                       (anf-of-simple-exp exp1)
+                       (anf-of-simple-exp exp2)))
+           (proc-exp (ids exp)
+                     (anf-proc-exp
+                      (append ids (list 'k%00))
+                      (anf-of-exp exp (anf-var-exp 'k%00))))
+           (sum-exp (exps)
+                    (anf-sum-exp (map anf-of-simple-exp exps)))
+           (else
+            (report-invalid-exp-to-anf-of-simple-exp exp)))))
+
+;;; make-send-to-cont : SimpleExp x SimpleExp -> TfExp
+(define make-send-to-cont
+  (lambda (k-exp s-exp)
+    (if (eq? k-exp end-cont)
+        (simple-exp->exp (anf-simple-exp->exp s-exp))
+        (cases simple-exp k-exp
+               (anf-proc-exp
+                (vars body)
+                (anf-let-exp
+                 (list (car vars))
+                 (list (anf-simple-exp->exp s-exp))
+                 body))
+               (else
+                (report-invalid-cont-exp k-exp))))))
+
+;;; make-call-send-to-cont : SimpleExp x CallExp -> TfExp
+(define make-call-send-to-cont
+  (lambda (k-exp c-exp)
+    (if (eq? k-exp end-cont)
+        (simple-exp->exp (anf-call-exp->exp c-exp))
+        (cases simple-exp k-exp
+               (anf-proc-exp
+                (vars body)
+                (anf-let-exp
+                 (list (car vars))
+                 (list (anf-call-exp->exp c-exp))
+                 body))
+               (else
+                (report-invalid-cont-exp k-exp))))))
+
+(define n 'uninitiated)
+(define fresh-identifier
+  (lambda (v)
+    (set! n (+ n 1))
+    (string->symbol (string-append (symbol->string v) (number->string n)))))
+
+(define report-invalid-exp-to-anf-of-simple-exp
+  (lambda (exp)
+    (eopl:error "invalid exp to anf: ~a~%" exp)))
+
+(define report-invalid-cont-exp
+  (lambda (exp)
+    (eopl:error "invalid cont exp: ~a~%" exp)))
