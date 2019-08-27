eb1c3f4f13f8c04df5cc912386e0e9f5872b967d
chihyang <chihyanghsin@gmail.com>
2019-08-28 01:50:21 +0900

    Exercise 5.39.

---
+++ ch06/exer6.39.letcc-tests.scm
@@ -0,0 +1,17 @@
+(module tests mzscheme
+
+  ;; tests for cps converter, including explicit references.
+
+  (provide letcc-test-list)
+  ;;;;;;;;;;;;;;;; tests ;;;;;;;;;;;;;;;;
+
+  (define letcc-test-list
+    '((
+       letcc-to-mimic-try
+       "letcc final-cnt in
+   -(letcc cnt in
+       throw let x = 3 in
+             -(3, -(2, throw x to cnt))
+       to final-cnt,
+     3)"
+       0))))
