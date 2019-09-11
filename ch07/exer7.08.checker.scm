dd70114e4ba8e37f4d3dc3c8f0453927f24bdf18
chihyang <chihyanghsin@gmail.com>
2019-09-11 21:54:22 +0800

    Exercise 7.8.

---
+++ ch07/exer7.08.checker.scm
@@ -0,0 +1,216 @@
+#lang eopl
+(require "exer7.08.lang.scm")
+(provide checked-type-of type-of type-of-program type-of-exp)
+
+;;; ---------------------- Type Environment ----------------------
+(define-datatype tenv tenv?
+  (empty-tenv)
+  (extend-tenv
+   (saved-var identifier?)
+   (saved-type type?)
+   (saved-tenv tenv?)))
+
+(define extend-tenv*
+  (lambda (vars types tenv)
+    (letrec ((extend-tenv*-inner
+              (lambda (vars types tenv)
+                (if (null? vars)
+                    tenv
+                    (extend-tenv*-inner (cdr vars)
+                                        (cdr types)
+                                        (extend-tenv (car vars) (car types) tenv))))))
+      (let ((var-len (length vars))
+            (type-len (length types)))
+        (cond [(< var-len type-len)
+               (report-type-var-number-mismatch 'greater)]
+              [(> var-len type-len)
+               (report-type-var-number-mismatch 'less)]
+              [else
+               (extend-tenv*-inner vars types tenv)])))))
+
+(define apply-tenv
+  (lambda (env var)
+    (cases tenv env
+           (empty-tenv
+            ()
+            (report-no-binding-type-found var))
+           (extend-tenv
+            (saved-var saved-type saved-tenv)
+            (if (eq? var saved-var)
+                saved-type
+                (apply-tenv saved-tenv var))))))
+
+(define report-no-binding-type-found
+  (lambda (search-var)
+    (eopl:error 'apply-tenv "No binding for ~s" search-var)))
+
+(define report-type-var-number-mismatch
+  (lambda (symp vars types)
+    (eopl:error 'extend-tenv* "type number is ~s than variable number in:~%~a, ~a"
+                symp vars (map type-to-external-form types))))
+
+;;; ---------------------- Type Checker ----------------------
+;;; check-equal-type! : Type x Type x Expression -> Unspecified
+(define check-equal-type!
+  (lambda (ty1 ty2 exp)
+    (when (not (equal? ty1 ty2))
+      (report-unequal-types ty1 ty2 exp))))
+;;; check-equal-types! : Listof(Type) x Listof(Type) x Expression -> Unspecified
+(define check-equal-types!
+  (lambda (tys1 tys2 exp)
+    (if (null? tys1)
+        #t
+        (if (equal? (car tys1) (car tys2))
+            (check-equal-types! (cdr tys1) (cdr tys2) exp)
+            (report-unequal-types (car tys1) (car tys2) exp)))))
+;;; report-unequal-types : Type x Type x Expression -> Unspecified
+(define report-unequal-types
+  (lambda (ty1 ty2 exp)
+    (eopl:error 'check-equal-type!
+                "Types didn't match: ~s != ~s in ~%~a"
+                (type-to-external-form ty1)
+                (type-to-external-form ty2)
+                exp)))
+(define type-to-external-form
+  (lambda (ty)
+    (cases type ty
+           (int-type () 'int)
+           (bool-type () 'bool)
+           (proc-type (arg-types result-type)
+                      (list (map type-to-external-form arg-types)
+                            '->
+                            (type-to-external-form result-type)))
+           (pair-type
+            (ty1 ty2)
+            (list 'pairof
+                  (type-to-external-form ty1)
+                  (type-to-external-form ty2))))))
+
+(define type-of-program
+  (lambda (prgm)
+    (cases program prgm
+           (a-program
+            (exp)
+            (type-of-exp exp (empty-tenv))))))
+
+(define type-of-exp
+  (lambda (exp tenv)
+    (cases expression exp
+           (const-exp
+            (num)
+            (int-type))
+           (var-exp
+            (var)
+            (apply-tenv tenv var))
+           (zero?-exp
+            (exp1)
+            (check-equal-type!
+             (int-type)
+             (type-of-exp exp1 tenv)
+             exp1)
+            (bool-type))
+           (diff-exp
+            (exp1 exp2)
+            (check-equal-type!
+             (int-type)
+             (type-of-exp exp1 tenv)
+             exp1)
+            (check-equal-type!
+             (int-type)
+             (type-of-exp exp2 tenv)
+             exp2)
+            (int-type))
+           (if-exp
+            (exp1 exp2 exp3)
+            (let ((ty1 (type-of-exp exp1 tenv))
+                  (ty2 (type-of-exp exp2 tenv))
+                  (ty3 (type-of-exp exp3 tenv)))
+              (check-equal-type! (bool-type) ty1 exp1)
+              (check-equal-type! ty2 ty3 exp)
+              ty2))
+           (proc-exp
+            (vars tys body)
+            (let ((result-type (type-of-exp body (extend-tenv* vars tys tenv))))
+              (proc-type tys result-type)))
+           (let-exp
+            (b-vars b-exps let-body)
+            (let ((b-types (type-of-exps b-exps tenv)))
+              (type-of-exp let-body (extend-tenv* b-vars b-types tenv))))
+           (letrec-exp
+            (result-types p-names b-vars arg-types b-bodies letrec-body)
+            (let ((letrec-tenv
+                   (extend-tenv*
+                    p-names
+                    (map proc-type arg-types result-types)
+                    tenv)))
+              (let ((checked-types
+                     (map (lambda (b-body b-var arg-type)
+                            (type-of-exp
+                             b-body
+                             (extend-tenv* b-var arg-type letrec-tenv)))
+                          b-bodies
+                          b-vars
+                          arg-types)))
+                (check-equal-types! checked-types result-types exp)
+                (type-of-exp letrec-body letrec-tenv))))
+           (call-exp
+            (rator rands)
+            (let ((rator-type (type-of-exp rator tenv))
+                  (rand-types (type-of-exps rands tenv)))
+              (cases type rator-type
+                     (proc-type
+                      (arg-types result-type)
+                      (check-equal-types! arg-types rand-types exp)
+                      result-type)
+                     (else
+                      (report-rator-not-a-proc-type rator-type rator)))))
+           (pair-exp
+            (exp1 exp2)
+            (let ((ty1 (type-of-exp exp1 tenv))
+                  (ty2 (type-of-exp exp2 tenv)))
+              (pair-type ty1 ty2)))
+           (unpair-exp
+            (var1 var2 exp1 body)
+            (let ((pt (type-of-exp exp1 tenv)))
+              (cases type pt
+                     (pair-type
+                      (ty1 ty2)
+                      (type-of-exp body
+                                   (extend-tenv* (list var1 var2)
+                                                 (list ty1 ty2)
+                                                 tenv)))
+                     (else
+                      (report-not-a-pair-type pt exp1))))))))
+
+(define report-rator-not-a-proc-type
+  (lambda (ty1 exp)
+    (eopl:error 'type-of-exp
+                "Expect a procedure, actual ~a: ~a"
+                (type-to-external-form ty1)
+                exp)))
+
+(define report-not-a-pair-type
+  (lambda (ty1 exp)
+    (eopl:error 'type-of-exp
+                "Expect a pair, actual ~a: ~a"
+                (type-to-external-form ty1)
+                exp)))
+
+(define type-of-exps
+  (lambda (exps tenv)
+    (if (null? exps)
+        '()
+        (cons (type-of-exp (car exps) tenv)
+              (type-of-exps (cdr exps) tenv)))))
+
+(define type-of
+  (lambda (prgm)
+    (type-to-external-form (type-of-program prgm))))
+
+;;; checked-type-of : String -> Type | String (for exception)
+(require (only-in racket/base with-handlers exn:fail?))
+(define checked-type-of
+  (lambda (prgm)
+    (with-handlers
+        [(exn:fail? (lambda (en) 'error))]
+      (type-of prgm))))
---
+++ ch07/exer7.08.interp.scm
@@ -0,0 +1,88 @@
+#lang eopl
+(require "exer7.08.lang.scm")
+(require "exer7.08.env.scm")
+(require "exer7.08.checker.scm")
+(provide value-of-program value-of run checked-run)
+
+;;; ---------------------- Evaluate expression ----------------------
+(define apply-procedure
+  (lambda (proc1 vals)
+    (cases proc proc1
+           (procedure
+            (vars body saved-env)
+            (value-of body (extend-env* vars vals saved-env))))))
+
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+           (const-exp
+            (num)
+            (num-val num))
+           (var-exp
+            (var)
+            (apply-env env var))
+           (diff-exp
+            (exp1 exp2)
+            (num-val (- (expval->num (value-of exp1 env))
+                        (expval->num (value-of exp2 env)))))
+           (zero?-exp
+            (exp1)
+            (bool-val (eqv? (expval->num (value-of exp1 env)) 0)))
+           (if-exp
+            (exp1 exp2 exp3)
+            (if (expval->bool (value-of exp1 env))
+                (value-of exp2 env)
+                (value-of exp3 env)))
+           (let-exp
+            (vars exps body)
+            (let ((vals (map (lambda (e) (value-of e env)) exps)))
+              (value-of body (extend-env* vars vals env))))
+           (letrec-exp
+            (p-result-types p-names p-vars p-arg-types p-bodies letrec-body)
+            (value-of letrec-body (extend-env-rec p-names p-vars p-bodies env)))
+           (proc-exp
+            (vars var-types body)
+            (proc-val (procedure vars body env)))
+           (call-exp
+            (rator rands)
+            (let ((proc (expval->proc (value-of rator env)))
+                  (args (map (lambda (e) (value-of e env)) rands)))
+              (apply-procedure proc args)))
+           (pair-exp
+            (exp1 exp2)
+            (pair-val (value-of exp1 env) (value-of exp2 env)))
+           (unpair-exp
+            (var1 var2 exp1 body)
+            (let ((pv (value-of exp1 env)))
+              (cases exp-val pv
+                     (pair-val
+                      (val1 val2)
+                      (value-of
+                       body
+                       (extend-env*
+                        (list var1 var2)
+                        (list val1 val2)
+                        env)))
+                     (else
+                      (report-invalid-exp-value 'pair))))))))
+
+;; value-of-program : Program -> SchemeVal
+(define value-of-program
+  (lambda (prog)
+    (cases program prog
+           (a-program
+            (exp)
+            (let ((val (value-of exp (empty-env))))
+              (expval->schemeval val))))))
+
+(define run
+  (lambda (prgm)
+    (value-of-program prgm)))
+
+;;; checked-run : String -> Int | Bool | Proc | String (for exception)
+(require (only-in racket/base with-handlers exn:fail?))
+(define checked-run
+  (lambda (prgm)
+    (with-handlers
+        [(exn:fail? (lambda (en) 'error))]
+      (run prgm))))
