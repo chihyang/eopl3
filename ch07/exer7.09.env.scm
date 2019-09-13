7010164b860541726a01e038dd4274c1c11d8c9d
chihyang <chihyanghsin@gmail.com>
2019-09-13 10:49:36 +0800

    Interpreter part of exercise 7.9.

---
+++ ch07/exer7.09.env.scm
@@ -0,0 +1,199 @@
+#lang eopl
+(require "exer7.09.lang.scm")
+(provide (all-defined-out))
+;;; ---------------------- Environment(from section 3.2) ----------------------
+(define member?
+  (lambda (sym lst)
+    (if (null? lst)
+        #f
+        (or (eqv? sym (car lst))
+            (member? sym (cdr lst))))))
+(define check-duplicates
+  (lambda (lst)
+    (cond ((null? lst) '())
+          ((member? (car lst) (cdr lst)) (car lst))
+          (else (check-duplicates (cdr lst))))))
+(define empty-env?
+  (lambda (env)
+    (and (list? env)
+         (not (null? env))
+         (eqv? (car env) 'empty-env))))
+(define extended-env?
+  (lambda (env)
+    (and (list? env)
+         (not (null? env))
+         (or (eqv? (car env) 'extend-env)
+             (eqv? (car env) 'extend-env-rec)))))
+(define environment?
+  (lambda (env)
+    (or (empty-env? env)
+        (extended-env? env))))
+(define empty-env
+  (lambda () (list 'empty-env)))
+(define extend-env
+  (lambda (var val env)
+    (list 'extend-env var val env)))
+(define extend-env-rec
+  (lambda (p-names p-vars p-bodies env)
+    (let ((dup-name (check-duplicates p-names)))
+      (if (null? dup-name)
+          (list 'extend-env-rec (list p-names p-vars p-bodies) env)
+          (report-duplicate-id dup-name)))))
+;; extend-env* : Listof(Id) x Listof(ExpVal) x Env -> Env
+(define extend-env*
+  (lambda (vars vals env)
+    (let ((dup (check-duplicates vars))
+          (var-len (length vars))
+          (val-len (length vals)))
+      (cond [(not (null? dup)) (report-duplicate-id dup)]
+            [(< var-len val-len)
+             (report-argument-mismatch 'greater)]
+            [(> var-len val-len)
+             (report-argument-mismatch 'less)]
+            [else
+             (letrec ((extend-env*-inner
+                       (lambda (vars vals env)
+                         (cond [(null? vars)
+                                env]
+                               [else
+                                (extend-env*-inner
+                                 (cdr vars)
+                                 (cdr vals)
+                                 (list 'extend-env (car vars) (car vals) env))]))))
+               (extend-env*-inner vars vals env))]))))
+(define apply-env
+  (lambda (env search-var)
+    (cond
+     ((eqv? (car env) 'empty-env)
+      (report-no-binding-found search-var))
+     ((eqv? (car env) 'extend-env)
+      (let ((saved-var (cadr env))
+            (saved-val (caddr env))
+            (saved-env (cadddr env)))
+        (if (eqv? search-var saved-var)
+            saved-val
+            (apply-env saved-env search-var))))
+     ((eqv? (car env) 'extend-env-rec)
+      (let ((func (apply-env-rec search-var
+                                 (car (cadr env))
+                                 (cadr (cadr env))
+                                 (caddr (cadr env)))))
+        (if (null? func)
+            (apply-env (caddr env) search-var)
+            (let ((saved-p-vars (car func))
+                  (saved-p-body (cadr func)))
+              (proc-val (procedure saved-p-vars saved-p-body env))))))
+     (else
+      (report-invalid-env env)))))
+(define apply-env-rec
+  (lambda (var p-names p-vars p-bodies)
+    (cond [(null? p-names) '()]
+          [(eqv? var (car p-names))
+           (list (car p-vars) (car p-bodies))]
+          [else
+           (apply-env-rec var (cdr p-names) (cdr p-vars) (cdr p-bodies))])))
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env "No binding for ~s" search-var)))
+(define report-invalid-env
+  (lambda (env)
+    (eopl:error 'apply-env "Bad environment: ~s" env)))
+(define report-argument-mismatch
+  (lambda (symp)
+    (eopl:error 'extend-env* "Argument number is ~s than parameter number" symp)))
+(define report-duplicate-id
+  (lambda (sym)
+    (eopl:error 'extend-env* "Duplicate identifier ~s" sym)))
+
+;;; ---------------------- Expval ----------------------
+(define-datatype proc proc?
+  (procedure
+   (vars (list-of identifier?))
+   (body expression?)
+   (saved-env environment?)))
+(define-datatype exp-val exp-val?
+  (num-val
+   (val number?))
+  (bool-val
+   (val boolean?))
+  (proc-val
+   (val proc?))
+  (null-val)
+  (pair-val
+   (val1 exp-val?)
+   (val2 exp-val?)))
+
+;; expval->schemeval : ExpVal -> SchemeVal
+(define expval->schemeval
+  (lambda (v)
+    (cases exp-val v
+           (num-val
+            (num)
+            num)
+           (bool-val
+            (bool)
+            bool)
+           (proc-val
+            (p)
+            (cases proc p
+                   (procedure
+                    (var saved-env body)
+                    `(λ (,var) ...))))
+           (null-val
+            ()
+            '())
+           (pair-val
+            (val1 val2)
+            `(,(expval->schemeval val1) .
+              ,(expval->schemeval val2))))))
+
+(define expval->num
+  (lambda (value)
+    (cases exp-val value
+           (num-val
+            (number)
+            number)
+           (else
+            (report-invalid-exp-value 'num)))))
+(define expval->bool
+  (lambda (value)
+    (cases exp-val value
+           (bool-val
+            (boolean)
+            boolean)
+           (else
+            (report-invalid-exp-value 'bool)))))
+(define expval->proc
+  (lambda (value)
+    (cases exp-val value
+           (proc-val
+            (proc1)
+            proc1)
+           (else
+            (report-invalid-exp-value 'proc)))))
+(define expval->pair
+  (lambda (value)
+    (cases exp-val value
+           (pair-val
+            (val1 val2)
+            (cons
+             (cases exp-val val1
+                    (num-val (num) num)
+                    (bool-val (bool) bool)
+                    (proc-val (proc1) proc1)
+                    (null-val () '())
+                    (pair-val (val3 val4) (expval->pair val1)))
+             (cases exp-val val2
+                    (num-val (num) num)
+                    (bool-val (bool) bool)
+                    (proc-val (proc1) proc1)
+                    (null-val () '())
+                    (pair-val (val3 val4) (expval->pair val2)))))
+           (else
+            (report-invalid-exp-value 'pair)))))
+
+(define report-invalid-exp-value
+  (lambda (type)
+    (eopl:error
+     'exp-val
+     "Not a valid exp value of type ~s" type)))
---
+++ ch07/exer7.09.interp.scm
@@ -0,0 +1,117 @@
+#lang eopl
+(require "exer7.09.lang.scm")
+(require "exer7.09.env.scm")
+(require "exer7.09.checker.scm")
+(provide value-of-program value-of run checked-run)
+
+;;; ---------------------- Evaluate expression ----------------------
+(define apply-procedure
+  (lambda (proc1 vals)
+    (cases proc proc1
+           (procedure
+            (vars body saved-env)
+            (value-of body (extend-env* vars vals saved-env))))))
+
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+           (const-exp
+            (num)
+            (num-val num))
+           (var-exp
+            (var)
+            (apply-env env var))
+           (diff-exp
+            (exp1 exp2)
+            (num-val (- (expval->num (value-of exp1 env))
+                        (expval->num (value-of exp2 env)))))
+           (zero?-exp
+            (exp1)
+            (bool-val (eqv? (expval->num (value-of exp1 env)) 0)))
+           (if-exp
+            (exp1 exp2 exp3)
+            (if (expval->bool (value-of exp1 env))
+                (value-of exp2 env)
+                (value-of exp3 env)))
+           (let-exp
+            (vars exps body)
+            (let ((vals (map (lambda (e) (value-of e env)) exps)))
+              (value-of body (extend-env* vars vals env))))
+           (letrec-exp
+            (p-result-types p-names p-vars p-arg-types p-bodies letrec-body)
+            (value-of letrec-body (extend-env-rec p-names p-vars p-bodies env)))
+           (proc-exp
+            (vars var-types body)
+            (proc-val (procedure vars body env)))
+           (call-exp
+            (rator rands)
+            (let ((proc (expval->proc (value-of rator env)))
+                  (args (map (lambda (e) (value-of e env)) rands)))
+              (apply-procedure proc args)))
+           (list-exp
+            (exp1 exps)
+            (let ((val1 (value-of exp1 env)))
+              (if (null? exps)
+                  (pair-val val1 (null-val))
+                  (pair-val val1
+                            (value-of (list-exp (car exps) (cdr exps)) env)))))
+           (cons-exp
+            (exp1 exp2)
+            (let ((val1 (value-of exp1 env))
+                  (val2 (value-of exp2 env)))
+              (pair-val
+               val1
+               (pair-val
+                val2
+                (null-val)))))
+           (car-exp
+            (exp1)
+            (let ((val1 (value-of exp1 env)))
+              (cases exp-val val1
+                     (pair-val
+                      (first rest)
+                      first)
+                     (else
+                      (report-invalid-exp-value 'list)))))
+           (cdr-exp
+            (exp1)
+            (let ((val1 (value-of exp1 env)))
+              (cases exp-val val1
+                     (pair-val
+                      (first rest)
+                      rest)
+                     (else
+                      (report-invalid-exp-value 'list)))))
+           (null-exp
+            (exp1)
+            (let ((val1 (value-of exp1 env)))
+              (cases exp-val val1
+                     (null-val
+                      ()
+                      #t)
+                     (else
+                      #f))))
+           (emptylist-exp
+            (ty)
+            (null-val)))))
+
+;; value-of-program : Program -> SchemeVal
+(define value-of-program
+  (lambda (prog)
+    (cases program prog
+           (a-program
+            (exp)
+            (let ((val (value-of exp (empty-env))))
+              (expval->schemeval val))))))
+
+(define run
+  (lambda (prgm)
+    (value-of-program prgm)))
+
+;;; checked-run : String -> Int | Bool | Proc | String (for exception)
+(require (only-in racket/base with-handlers exn:fail?))
+(define checked-run
+  (lambda (prgm)
+    (with-handlers
+        [(exn:fail? (lambda (en) 'error))]
+      (run prgm))))
