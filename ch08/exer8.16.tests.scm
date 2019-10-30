4cfbbe980125c130b81fd914938cc58cac3b4bb2
chihyang <chihyanghsin@gmail.com>
2019-10-30 23:29:59 +0800

    Parser of exercise 8.16.

---
+++ ch08/exer8.16.tests.scm
@@ -0,0 +1,30 @@
+#lang eopl
+(require "exer8.16.lang.scm")
+(require rackunit)
+(require (only-in "exer8.16.module-tests.scm"
+                  tests-for-parse
+                  tests-for-run
+                  tests-for-check))
+
+(define test-name car)
+(define test-program cadr)
+(define test-answer caddr)
+(define passed 0)
+(define failed 0)
+
+(for-each
+ (lambda (test)
+   (let ((v1 (test-scan&parse (test-program test)))
+         (v2 (test-answer test)))
+     (if  (equal? v1 v2)
+          (begin
+            (set! passed (+ passed 1)))
+          (begin
+            (set! failed (+ failed 1))
+            (eopl:printf "test for parsing ~a failed: expect ~a, actual ~a~%"
+                         (test-name test) v2 v1)))))
+ tests-for-parse)
+
+(if (eq? passed (length tests-for-parse))
+    (eopl:printf "all tests passed!~%")
+    (eopl:printf "~%~a tests failed!~%" failed))
