41392f30415f6e53731d2a307639f04cd5c205c4
chihyang <chihyanghsin@gmail.com>
2019-11-10 23:07:26 +0800

    Add tests for interpreter for section 8.3.

--- ch08/chap08.s03.tests.scm
+++ ch08/chap08.s03.tests.scm
@@ -1,5 +1,6 @@
 #lang eopl
 (require "chap08.s03.lang.scm")
+(require "chap08.s03.interp.scm")
 (require rackunit)
 (require (only-in "exer8.19-23.scm"
                   [tests-for-parse tests-for-parse-exer8.19-23]
@@ -29,6 +30,20 @@
                          (test-name test) v2 v1)))))
  tests-for-parse)
 
-(if (eq? passed (+ (length tests-for-parse)))
+(for-each
+ (lambda (test)
+   (let ((v1 (checked-run
+              (checked-scan&parse (test-program test))))
+         (v2 (test-answer test)))
+     (if  (equal? v1 v2)
+          (begin
+            (set! passed (+ passed 1)))
+          (begin
+            (set! failed (+ failed 1))
+            (eopl:printf "test for ~a failed: expect ~a, actual ~a~%"
+                         (test-name test) v2 v1)))))
+ tests-for-run)
+
+(if (eq? passed (+ (length tests-for-parse) (length tests-for-run)))
     (eopl:printf "all tests passed!~%")
     (eopl:printf "~%~a tests failed!~%" failed))
