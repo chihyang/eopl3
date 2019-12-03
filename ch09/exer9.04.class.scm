93bd4541934401c080413709fa0a54e146de64d3
chihyang <chihyanghsin@gmail.com>
2019-12-03 11:06:05 +0800

    Exercise 9.4.

---
+++ ch09/exer9.04.class.scm
@@ -0,0 +1,161 @@
+#lang eopl
+(require "chap09.s03.lang.scm")
+(require "chap09.s03.store.scm")
+(provide (all-defined-out))
+
+;;; ---------------------- Method ----------------------
+(define-datatype method method?
+  (a-method
+   (vars (list-of identifier?))
+   (body expression?)
+   (super-name identifier?)
+   (field-names (list-of identifier?))))
+
+;;; ---------------------- Class Environment ----------------------
+;;; ClassEnv = Listof(Listof(ClassName, Class))
+;;; the-class-env : ClassEnv
+(define the-class-env '())
+
+;;; add-to-class-env! : ClassName x Class -> Unspecified
+(define add-to-class-env!
+  (lambda (class-name class)
+    (set! the-class-env
+      (cons (list class-name class) the-class-env))))
+
+;;; lookup-class : ClassName -> Class
+(define lookup-class
+  (lambda (name)
+    (let ((maybe-pair (assq name the-class-env)))
+      (if maybe-pair
+          (cadr maybe-pair)
+          (report-unknown-class name)))))
+
+(define report-unknown-class
+  (lambda (name)
+    (eopl:error 'lookup-class "Unknown class ~s" name)))
+
+;;; ---------------------- Class ----------------------
+(define-datatype class class?
+  (a-class
+   (super-name (maybe identifier?))
+   (field-names (list-of identifier?))
+   (method-env method-environment?)))
+
+;;; class->field-names : Class -> Listof(Sym)
+(define class->field-names
+  (lambda (c)
+    (cases class c
+           (a-class
+            (s-name f-names m-env) f-names))))
+
+;;; class->field-names : Class -> MethodEnv
+(define class->method-env
+  (lambda (c)
+    (cases class c
+           (a-class
+            (s-name f-names m-env) m-env))))
+
+;;; maybe : Pred -> Pred
+(define maybe
+  (lambda (foo)
+    (lambda (x)
+      (or (foo x) (eqv? x #f)))))
+
+;;; initialize-class-env! : Listof(ClassDecl) -> Unspecified
+(define initialize-class-env!
+  (lambda (c-decls)
+    (set! identifier-counter 0)
+    (set! the-class-env
+      (list
+       (list 'object (a-class #f '() '()))))
+    (for-each initialize-class-decl! c-decls)))
+
+;;; initialize-class-decl! : ClassDecl -> Unspecified
+(define initialize-class-decl!
+  (lambda (c-decl)
+    (cases class-decl c-decl
+           (a-class-decl
+            (c-name s-name f-names m-decls)
+            (let ((f-names
+                   (append-field-names
+                    (class->field-names (lookup-class s-name))
+                    f-names)))
+              (add-to-class-env!
+               c-name
+               (a-class s-name f-names
+                        (merge-method-envs
+                         (class->method-env (lookup-class s-name))
+                         (method-decls->method-env
+                          m-decls s-name f-names)))))))))
+
+;;; append-field-names : Listof(FieldName) x Listof(FieldName) -> Listof(FieldName)
+(define append-field-names
+  (lambda (super-fields new-fields)
+    (cond [(null? super-fields) new-fields]
+          [else
+           (cons
+            (if (memq (car super-fields) new-fields)
+                (fresh-identifier (car super-fields))
+                (car super-fields))
+            (append-field-names
+             (cdr super-fields) new-fields))])))
+
+;;; fresh-identifier from chapter 6
+(define identifier-counter 'uninitiated)
+(define fresh-identifier
+  (lambda (v)
+    (set! identifier-counter (+ identifier-counter 1))
+    (string->symbol (string-append (symbol->string v) (number->string identifier-counter)))))
+
+;;; ---------------------- Method Environment ----------------------
+;;; MethodEnv = Listof(List(MethodName, Method))
+;;; method-environment? : SchemeVal -> Bool
+(define method-environment?
+  (lambda (val)
+    (list-of method-pair?)))
+
+;;; method-pair? : SchemeVal -> Bool
+(define method-pair?
+  (lambda (val)
+    (if (pair? val)
+        (and (identifier? (car val))
+             (method? (cadr val))
+             (null? (cddr val)))
+        #f)))
+
+;;; find-method : Sym x Sym -> Method
+(define find-method
+  (lambda (c-name name)
+    (let ((clss (lookup-class c-name)))
+      (find-method-from-class clss name))))
+
+;;; find-method-from-class : Class x Sym -> Method
+(define find-method-from-class
+  (lambda (clss name)
+    (let ((m-env (class->method-env clss)))
+      (let ((maybe-pair (assq name m-env)))
+        (if maybe-pair
+            (cadr maybe-pair)
+            (report-method-not-found clss name))))))
+
+(define report-method-not-found
+  (lambda (c-name m-name)
+    (eopl:error 'find-method "Unknown method ~s in class ~s" m-name c-name)))
+
+;;; method-decls->method-env : Listof(MethodDecl) x ClassName x
+;;; Listof(FieldName) -> MethodEnv
+(define method-decls->method-env
+  (lambda (m-decls super-name field-names)
+    (map
+     (lambda (m-decl)
+       (cases method-decl m-decl
+              (a-method-decl
+               (m-name vars body)
+               (list m-name
+                     (a-method vars body super-name field-names)))))
+     m-decls)))
+
+;;; merge-method-envs : MethodEnv x MethodEnv -> MethodEnv
+(define merge-method-envs
+  (lambda (super-m-env self-m-env)
+    (append self-m-env super-m-env)))
---
+++ ch09/exer9.04.interp.scm
@@ -0,0 +1,203 @@
+#lang eopl
+(require "chap09.s03.lang.scm")
+(require "chap09.s03.store.scm")
+(require "exer9.04.env.scm")
+(require "exer9.04.class.scm")
+(provide value-of-program value-of run checked-run)
+
+;;; ---------------------- Object ----------------------
+;;; new-object : ClassName (= Sym) -> Obj
+(define new-object
+  (lambda (class-name)
+    (an-object
+     (lookup-class class-name)
+     (map (lambda (field-name)
+            ;; in fact, this is surplus, just the code below is okay:
+            ;; (newref (list 'uninitialized-field field-name))
+            (newref 'uninitialized-field)
+            )
+          (class->field-names (lookup-class class-name))))))
+
+;;; ---------------------- Evaluate expression ----------------------
+(define apply-procedure
+  (lambda (proc1 vals)
+    (cases proc proc1
+           (procedure
+            (vars body saved-env)
+            (value-of body (extend-env* vars (map newref vals) saved-env))))))
+
+;;; apply-method : Method x Obj x Listof(ExpVal) -> ExpVal
+(define apply-method
+  (lambda (m self args)
+    (cases method m
+           (a-method
+            (vars body super-name field-names)
+            (value-of
+             body
+             (extend-env* vars (map newref args)
+                          (extend-env-with-self-and-super
+                           (newref (obj-val self)) super-name
+                           ;; Why doesn't this cause problem even if
+                           ;;
+                           ;; (< (length field-names) (length (object->fields self))
+                           ;;
+                           ;; Because fields corresponding to field-names are
+                           ;; always a subset of (object->fields self), IN
+                           ;; ORDER. This is why the fields in a class is
+                           ;; arranged in the order specified on page 341
+                           (extend-env* field-names
+                                        (object->fields self)
+                                        (empty-env)))))))))
+
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+           (const-exp
+            (num)
+            (num-val num))
+           (var-exp
+            (var)
+            (deref (apply-env env var)))
+           (emptylist-exp
+            ()
+            (null-val))
+           (cons-exp
+            (exp1 exp2)
+            (let ((val1 (value-of exp1 env))
+                  (val2 (value-of exp2 env)))
+              (pair-val val1 val2)))
+           (car-exp
+            (exp1)
+            (let ((val1 (value-of exp1 env)))
+              (cases exp-val val1
+                     (pair-val
+                      (first rest)
+                      first)
+                     (else
+                      (report-invalid-exp-value 'list)))))
+           (cdr-exp
+            (exp1)
+            (let ((val1 (value-of exp1 env)))
+              (cases exp-val val1
+                     (pair-val
+                      (first rest)
+                      rest)
+                     (else
+                      (report-invalid-exp-value 'list)))))
+           (null?-exp
+            (exp1)
+            (let ((val1 (value-of exp1 env)))
+              (cases exp-val val1
+                     (null-val
+                      ()
+                      (bool-val #t))
+                     (else
+                      (bool-val #f)))))
+           (list-exp
+            (exp1)
+            (if (null? exp1)
+                (null-val)
+                (pair-val (value-of (car exp1) env)
+                          (value-of (list-exp (cdr exp1)) env))))
+           (diff-exp
+            (exp1 exp2)
+            (num-val (- (expval->num (value-of exp1 env))
+                        (expval->num (value-of exp2 env)))))
+           (sum-exp
+            (exp1 exp2)
+            (num-val (+ (expval->num (value-of exp1 env))
+                        (expval->num (value-of exp2 env)))))
+           (zero?-exp
+            (exp1)
+            (bool-val (eqv? (expval->num (value-of exp1 env)) 0)))
+           (if-exp
+            (exp1 exp2 exp3)
+            (if (expval->bool (value-of exp1 env))
+                (value-of exp2 env)
+                (value-of exp3 env)))
+           (let-exp
+            (vars exps body)
+            (let ((vals (map (lambda (e) (value-of e env)) exps)))
+              (value-of body (extend-env* vars (map newref vals) env))))
+           (letrec-exp
+            (p-names p-vars p-bodies letrec-body)
+            (value-of letrec-body (extend-env-rec p-names p-vars p-bodies env)))
+           (proc-exp
+            (vars body)
+            (proc-val (procedure vars body env)))
+           (call-exp
+            (rator rands)
+            (let ((proc (expval->proc (value-of rator env)))
+                  (args (map (lambda (e) (value-of e env)) rands)))
+              (apply-procedure proc args)))
+           (begin-exp
+            (exp1 exps)
+            (if (null? exps)
+                (value-of exp1 env)
+                (begin
+                  (value-of exp1 env)
+                  (value-of (begin-exp (car exps) (cdr exps)) env))))
+           (assign-exp
+            (var exp1)
+            (let ((ref (apply-env env var))
+                  (val (value-of exp1 env)))
+              (setref! ref val)
+              (num-val 27)))
+           (new-object-exp
+            (class-name rands)
+            (let ((args (value-of-exps rands env))
+                  (obj (new-object class-name)))
+              ;; (eopl:printf "apply method ~s ~s~%" class-name 'initialize)
+              (apply-method
+               (find-method class-name 'initialize)
+               obj
+               args)
+              (obj-val obj)))
+           (method-call-exp
+            (obj-exp method-name rands)
+            (let ((args (value-of-exps rands env))
+                  (obj (expval->obj (value-of obj-exp env))))
+              ;; (eopl:printf "apply method ~s ~s~%" (object->class-name obj) method-name)
+              (apply-method
+               (find-method-from-class (object->class obj) method-name)
+               obj
+               args)))
+           (super-call-exp
+            (method-name rands)
+            (let ((args (value-of-exps rands env))
+                  (obj (expval->obj (deref (apply-env env '%self)))))
+              ;; (eopl:printf "apply method ~s ~s~%" (object->class-name obj) method-name)
+              (apply-method
+               (find-method (apply-env env '%super) method-name)
+               obj
+               args)))
+           (self-exp
+            ()
+            (deref (apply-env env '%self))))))
+
+(define value-of-exps
+  (lambda (exps env)
+    (map (lambda (exp) (value-of exp env)) exps)))
+
+;; value-of-program : Program -> SchemeVal
+(define value-of-program
+  (lambda (prog)
+    (initialize-store!)
+    (cases program prog
+           (a-program
+            (class-decls exp)
+            (initialize-class-env! class-decls)
+            (let ((val (value-of exp (init-env))))
+              (expval->schemeval val))))))
+
+(define run
+  (lambda (prgm)
+    (value-of-program prgm)))
+
+;;; checked-run : String -> Int | Bool | Proc | String (for exception)
+(require (only-in racket/base with-handlers exn:fail?))
+(define checked-run
+  (lambda (prgm)
+    (with-handlers
+        [(exn:fail? (lambda (en) 'error))]
+      (run prgm))))
