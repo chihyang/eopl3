1f701f13ab0b97b058a9aacdd9909efdb0f0f767
chihyang <chihyanghsin@gmail.com>
2019-12-17 23:53:00 +0800

    Add test to exercise 9.13.

--- ch09/exer9.13.class-tests.scm
+++ ch09/exer9.13.class-tests.scm
@@ -680,6 +680,17 @@ in send o1 sum()
 "
      error)
 
+    (bogus-odd-even-via-self
+     "
+class oddeven extends object
+  method initialize()1
+  final method even(n)if zero?(n) then 1 else send self odd (-(n,1))
+  final method odd(n) if zero?(n) then 0 else send self even (-(n,1))
+class bogus-oddeven extends oddeven
+  method even(n) 0
+
+let o1 = new bogus-oddeven() in send o1 odd(13)"
+     error)
     )
 
   )
